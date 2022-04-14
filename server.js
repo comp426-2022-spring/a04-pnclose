c18abe3a73399eee3ebf8cce7f62d817eaf85d77
Peter Close <pclose99@gmail.com>
2022-04-14 12:14:17 -0400

    middleware set up

--- server.js
+++ server.js
@@ -56,6 +56,38 @@ if (args.help || args.h) {
 }
 
 
+
+// log using fs
+if (args.log == true) {
+    const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
+    app.use(morgan('combined', { stream: accessLog }))
+}
+
+// Set Up MiddleWare
+app.use((req, res, next) => {
+    let logdata = {
+        remoteaddr: req.ip,
+        remoteuser: req.user,
+        time: Date.now(),
+        method: req.method,
+        url: req.url,
+        protocol: req.protocol,
+        httpversion: req.httpVersion,
+        status: res.statusCode,
+        referer: req.headers["referer"],
+        useragent: req.headers["user-agent"],
+    };
+
+    const stmt = db.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+
+    next();
+});
+
+
+
+
+
 // ------------------------------------------------------------------------------
 // -------------------------------- COIN STUFF ----------------------------------
 // ------------------------------------------------------------------------------
